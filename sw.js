97d0c08a1c13cc41ded3bd7a08a6e2438d2b5669
CwryW <35494300+CwryW@users.noreply.github.com>
2019-08-28 08:53:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-importScripts('sw-cache-polyfill.js');
+importScripts('/cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('cwryw').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/style.css',
-       '/script.js', 
+      './index.html',
+      './style.css',
+      './script.js', 
      ]);
    })
  );
