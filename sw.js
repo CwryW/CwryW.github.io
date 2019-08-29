0ca4e20eb240c490b173639849f49c0ab07f5cb4
CwryW <35494300+CwryW@users.noreply.github.com>
2019-08-29 18:35:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,4 +16,14 @@ self.addEventListener('fetch', function(event) {
 
 console.log(event.request.url);
 
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
