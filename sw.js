de5044828b1c106a0697c9971d76117a4565c03b
CwryW <35494300+CwryW@users.noreply.github.com>
2019-08-29 19:00:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('cwryw').then(function(cache) {
      return cache.addAll([
-      '/',
-      '/index.html?homescreen=1',
+      '/', 
       '/?homescreen=1',
       '/index.html',
       '/style.css'
